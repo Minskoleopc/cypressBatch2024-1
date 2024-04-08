3c6d502c3cc89c92955635b34ef1ecea1d289f42
unknown <Minskoleopc>
2024-04-08 20:00:25 +0530

    added

--- cypress/e2e/3-basics/radioButtonCheckBoxDropDown.cy.js
+++ cypress/e2e/3-basics/radioButtonCheckBoxDropDown.cy.js
@@ -5,7 +5,7 @@ describe('verify the radio button', function () {
         cy.visit('https://webdriveruniversity.com/Dropdown-Checkboxes-RadioButtons/index.html')
     })
 
-    it.only('verify the radio button', function () {
+    it('verify the radio button', function () {
 
         //click()
         // cy.get('input[value="green"]').click()
@@ -23,15 +23,55 @@ describe('verify the radio button', function () {
 
     })
 
-    it.only('verify the functionality of check box', function () {
+    it('verify the functionality of check box with check', function () {
         cy.get('input[type="checkbox"]').each(function (el) {
             cy.wrap(el).check().should('be.checked')
             cy.wrap(el).uncheck().should('not.be.checked')
         })
+    })
 
+    it('verify the functionality of check box with loop', function () {
+        cy.get('input[type="checkbox"]').each(function (el) {
+            cy.wrap(el).check().should('be.checked')
+            cy.wrap(el).uncheck().should('not.be.checked')
+        })
+    })
 
+    it('verify the functionality of check box with click', function () {
+        cy.get('input[type="checkbox"]').each(function (el,index) {
+           if(index != 2){
+            cy.wrap(el).click()
+           }
+        })
+    })
+
+    it('verify the functionality of check box - multiple', function () {
+        cy.get('input[type="checkbox"]').check(['option-1','option-2','option-3','option-4'])
+    })
+
+    it('selecting the dropdown - one',function(){
+        // text
+        //cy.get('#dropdowm-menu-1').select('Python')
+        // value attribute
+        cy.get('#dropdowm-menu-1').select('python')
 
     })
+
+    it.only('selecting the dropdown - one',function(){
+        let selection = ["java","maven","css"]
+        cy.get('.section-title').first().find('select').each(function(el,index){
+            cy.wrap(el).select(selection[index])
+        })
+
+    })
+
+
+
+
+
+
+
+
 
 
 })
